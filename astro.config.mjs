857d52b0bf4e8e11e29114e765498c19defdcf11
Son Hong Su <son.ho463@gmail.com>
2023-02-22 11:27:42 +0900

    レイアウト

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-minimal-starter.netlify.app/",
-  integrations: [sitemap()]
+  integrations: [sitemap()],
 });
--- src/data/settings.js
+++ src/data/settings.js
@@ -1,13 +1,14 @@
 export default {
   title: `仕出し・弁当・オードブル　みっちゃん`,
   description: `This is an example blog made with Astro.`,
-  url: `https://astro-minimal-starter.netlify.app`, // No trailing slash!
+  url: `https://micchan.webson.site`,
+	 // No trailing slash!
   // JSON LD
-  name: `Example`,
+  name: `Micchan`
   // Facebook URL
-  facebookURL: `https://facebook.com`,
-  // Twitter URL
-  twitterURL: `https://twitter.com/jaydanurwin`,
-  // Instagram URL
-  instagramURL: `https://instagram.com/jaydanurwin`,
+  // facebookURL: `https://facebook.com`,
+  // // Twitter URL
+  // twitterURL: `https://twitter.com/jaydanurwin`,
+  // // Instagram URL
+  // instagramURL: `https://instagram.com/jaydanurwin`,
 };
