1b6be7b2614ccaa50cd17459d911c9ed5a5d329b
Son Hong Su <son.ho463@gmail.com>
2023-02-22 12:15:50 +0900

    FV

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,6 +1,6 @@
 export default {
   title: `仕出し・弁当・オードブル　みっちゃん`,
-  description: `This is an example blog made with Astro.`,
+  description: `沖縄県那覇市宇栄原、創業50年の仕出し、お弁当、オードブルのお店です。`,
   url: `https://micchan.webson.site`,
 	 // No trailing slash!
   // JSON LD
