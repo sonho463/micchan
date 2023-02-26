ac10c0a476b29ded9aeb57e000674d35a55b4813
Son Hong Su <son.ho463@gmail.com>
2023-02-26 14:01:25 +0900

    info

---
+++ src/data/menuUsual.js
@@ -0,0 +1,35 @@
+export default[
+	{
+		name: 'フーチャンプルーフーチャンプルー',
+		path: 'foo-champluu'
+	},
+	{
+		name: 'フーチャンプルー',
+		path: 'foo-champluu'
+	},
+	{
+		name: 'フーチャンプルー',
+		path: 'foo-champluu'
+	},
+	{
+		name: 'フーチャンプルー',
+		path: 'foo-champluu'
+	},
+	{
+		name: 'フーチャンプルー',
+		path: 'foo-champluu'
+	},
+	{
+		name: 'フーチャンプルー',
+		path: 'foo-champluu'
+	},
+	{
+		name: 'フーチャンプルー',
+		path: 'foo-champluu'
+	},
+	{
+		name: 'フーチャンプルー',
+		path: 'foo-champluu'
+	},
+
+]
