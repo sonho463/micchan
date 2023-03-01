d92bbc5493925177990dc9cf272d7f381a5b2c82
Son Hong Su <son.ho463@gmail.com>
2023-03-02 07:56:04 +0900

    スライダーのアイテム

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,16 @@
 // import Swiper bundle with all modules installed
-import Swiper from "swiper/bundle";
+import Swiper from 'swiper/bundle';
 
-console.log(Swiper);
+const textSlider = new Swiper('.js-text-slider',{
+	autoplay: true,
+	direction: 'horizontal',
+	effect: 'fade',
 
+})
+
+const imageSlider = new Swiper('.js-image-slider',{
+	effect: 'fade'
+})
+
+textSlider.controller.control = imageSlider
+imageSlider.controller.control = textSlider
