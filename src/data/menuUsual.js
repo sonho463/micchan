0e433ca8489a35ac7468391406b8fdf261b35adf
Son Hong Su <son.ho463@gmail.com>
2023-03-05 16:11:48 +0900

    いつものお弁当画像

--- src/data/menuUsual.js
+++ src/data/menuUsual.js
@@ -1,35 +1,66 @@
-export default[
-	{
-		name: 'フーチャンプルーフーチャンプルー',
-		path: 'foo-champluu'
-	},
-	{
-		name: 'フーチャンプルー',
-		path: 'foo-champluu'
-	},
-	{
-		name: 'フーチャンプルー',
-		path: 'foo-champluu'
-	},
-	{
-		name: 'フーチャンプルー',
-		path: 'foo-champluu'
-	},
-	{
-		name: 'フーチャンプルー',
-		path: 'foo-champluu'
-	},
-	{
-		name: 'フーチャンプルー',
-		path: 'foo-champluu'
-	},
-	{
-		name: 'フーチャンプルー',
-		path: 'foo-champluu'
-	},
-	{
-		name: 'フーチャンプルー',
-		path: 'foo-champluu'
-	},
-
-]
+export default [
+  {
+    name: "900円弁当",
+    path: "ben-900",
+  },
+  {
+    name: "日替わり",
+    path: "daily",
+  },
+  {
+    name: "白身魚フライ",
+    path: "flied-siromi",
+  },
+  {
+    name: "ふーちゃんぷるー",
+    path: "foo-champlu",
+  },
+  {
+    name: "ハンバーグ",
+    path: "hamburg",
+  },
+  {
+    name: "からあげ",
+    path: "kara-age",
+  },
+  {
+    name: "マーボーなす",
+    path: "mabo-nasu",
+  },
+  {
+    name: "中味汁",
+    path: "nakami",
+  },
+  {
+    name: "なすみそ",
+    path: "nasu-miso",
+  },
+  {
+    name: "煮つけ",
+    path: "nitsuke",
+  },
+  {
+    name: "ポークたまご",
+    path: "pork-egg",
+  },
+  {
+    name: "さば味噌",
+    path: "saba-miso",
+  },
+  {
+    name: "豆腐ちゃんぷるー",
+    path: "tofu-champlu",
+  },
+  {
+    name: "とんかつ",
+    path: "ton-katsu",
+  },
+  {
+    name: "焼き肉",
+    path: "yakiniku-box",
+  },
+  {
+    name: "焼き肉",
+    path: "yakiniku01",
+  },
+];
