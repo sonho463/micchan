ab0e95c0ca261fad1d95c9fddb3e986708705887
Son Hong Su <son.ho463@gmail.com>
2023-03-06 19:39:42 +0900

    ハンバーガーメニューの挙動

--- src/scripts/top.js
+++ src/scripts/top.js
@@ -12,3 +12,17 @@ hamburger.addEventListener("click", () => {
 		headerRight.classList.remove('open')
 	}
 });
+
+const linkButtonCollection= document.getElementsByTagName('a');
+const linkButtonArray = Array.from(linkButtonCollection)
+console.log(linkButtonArray)
+
+linkButtonArray.forEach((ele)=>{
+	ele.addEventListener('click',()=>{
+		console.log('click')
+		if(hamburger.classList.contains('open')){
+			hamburger.classList.remove('open')
+			headerRight.classList.remove('open')
+		}
+	})
+})
