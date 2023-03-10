6b7966cfc0cdd4afeca06ffdcc7eedeaae4422d1
Son Hong Su <son.ho463@gmail.com>
2023-03-10 17:52:20 +0900

    画像Component.src

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'; // import lit from '@astrojs/lit';
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'; // https://astro.build/config
 
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-minimal-starter.netlify.app/",
-  integrations: [sitemap()],
+  integrations: [sitemap(), image()]
 });
