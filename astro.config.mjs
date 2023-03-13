e0cc74f09cc9632edb616f733aea739c79e278d7
Son Hong Su <son.ho463@gmail.com>
2023-03-13 22:19:48 +0900

    pagination

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from 'astro/config'; // import lit from '@astrojs/lit';
-
-import sitemap from '@astrojs/sitemap'; // https://astro.build/config
-
+import { defineConfig } from "astro/config"; // import lit from '@astrojs/lit';
+import sitemap from "@astrojs/sitemap"; // https://astro.build/config
 import image from "@astrojs/image";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-minimal-starter.netlify.app/",
-  integrations: [sitemap(), image({
-		serviceEntryPoint: '@astrojs/image/sharp'
-	})],
-	// root: "./",
+  integrations: [
+    sitemap(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
