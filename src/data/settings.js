374a86be86ca7673ac97f533e150eb5550e023db
Son Hong Su <son.ho463@gmail.com>
2023-03-16 13:08:26 +0900

    desc

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,6 +1,6 @@
 export default {
   title: `仕出し・弁当・オードブル　みっちゃん`,
-  description: `沖縄県那覇市宇栄原、創業50年の仕出し、お弁当、オードブルのお店です。`,
+  description: `沖縄県那覇市宇栄で50年、仕出し、お弁当、オードブルのみちゃん弁当です。地元沖縄のおばー（おばあちゃん）たちの沖縄伝統料理を提供します。トゥシビーや結納のときのお料理、会議やイベントのお料理など対応できます。`,
   url: `https://micchan.webson.site`,
 	 // No trailing slash!
   // JSON LD
