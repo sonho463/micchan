b74944a056d9a27c2d0fae6e2c43ec727bf83ff7
Son Hong Su <son.ho463@gmail.com>
2023-04-24 15:54:36 +0900

    スライドスピード調整

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ import Swiper from 'swiper/bundle';
 
 const textSlider = new Swiper('.js-text-slider',{
 	autoplay: true,
+	speed: "1000",
+	delay: "1000",
 	direction: 'horizontal',
 	effect: 'fade',
 
