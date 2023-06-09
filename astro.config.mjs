6c128f6928b1c427f628514d157950879baeb062
sonho463 <son.ho463@gmail.com>
2023-06-09 17:15:58 +0900

    .srcを削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config"; // import lit from '@astrojs/lit';
 import sitemap from "@astrojs/sitemap"; // https://astro.build/config
-import image from "@astrojs/image";
 
 
 // https://astro.build/config
@@ -8,8 +7,5 @@ export default defineConfig({
   site: "https://micchan-bentou.com/",
   integrations: [
     sitemap(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
   ],
 });
